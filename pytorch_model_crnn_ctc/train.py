9317a884cde2a9176b714b6cb2f9bbe92a3fbc6b
hdh0809 <68471603+hdh0809@users.noreply.github.com>
2021-03-11 21:04:45 +0800

    Update train.py

--- pytorch_model_crnn_ctc/train.py
+++ pytorch_model_crnn_ctc/train.py
@@ -72,33 +72,127 @@ class FeatureMap(torch.nn.Module):
         out = torch.stack(tl, dim=1)
         return out
 
+class VGG16(nn.Module):
+    def __init__(self):
+        super(VGG16, self).__init__()
+        
+        # 3 * 224 * 224
+        self.conv1_1 = nn.Conv2d(3, 64, 3) # 64 * 222 * 222
+        self.conv1_2 = nn.Conv2d(64, 64, 3, padding=(1, 1)) # 64 * 222* 222
+        self.maxpool1 = nn.MaxPool2d((2, 2), padding=(1, 1)) # pooling 64 * 112 * 112
+        
+        self.conv2_1 = nn.Conv2d(64, 128, 3) # 128 * 110 * 110
+        self.conv2_2 = nn.Conv2d(128, 128, 3, padding=(1, 1)) # 128 * 110 * 110
+        self.maxpool2 = nn.MaxPool2d((2, 2), padding=(1, 1)) # pooling 128 * 56 * 56
+        
+        self.conv3_1 = nn.Conv2d(128, 256, 3) # 256 * 54 * 54
+        self.conv3_2 = nn.Conv2d(256, 256, 3, padding=(1, 1)) # 256 * 54 * 54
+        self.conv3_3 = nn.Conv2d(256, 256, 3, padding=(1, 1)) # 256 * 54 * 54
+        self.maxpool3 = nn.MaxPool2d((2, 2), padding=(1, 1)) # pooling 256 * 28 * 28
+        
+        self.conv4_1 = nn.Conv2d(256, 512, 3) # 512 * 26 * 26
+        self.conv4_2 = nn.Conv2d(512, 512, 3, padding=(1, 1)) # 512 * 26 * 26
+        self.conv4_3 = nn.Conv2d(512, 512, 3, padding=(1, 1)) # 512 * 26 * 26
+        self.maxpool4 = nn.MaxPool2d((2, 2), padding=(1, 1)) # pooling 512 * 14 * 14
+        
+        self.conv5_1 = nn.Conv2d(512, 512, 3) # 512 * 12 * 12
+        self.conv5_2 = nn.Conv2d(512, 512, 3, padding=(1, 1)) # 512 * 12 * 12
+        self.conv5_3 = nn.Conv2d(512, 512, 3, padding=(1, 1)) # 512 * 12 * 12
+        self.maxpool5 = nn.MaxPool2d((2, 2), padding=(1, 1)) # pooling 512 * 7 * 7
+        
+        # view
+        
+        # self.fc1 = nn.Linear(512 * 7 * 7, 4096)
+        # self.fc2 = nn.Linear(4096, 4096)
+        # self.fc3 = nn.Linear(4096, 1000)
+        # softmax 1 * 1 * 1000
+        
+    def forward(self, x):
+        
+        # x.size(0)即为batch_size
+        in_size = x.size(0)
+        
+        out = self.conv1_1(x) # 222
+        out = F.relu(out)
+        out = self.conv1_2(out) # 222
+        out = F.relu(out)
+        out = self.maxpool1(out) # 112
+        
+        out = self.conv2_1(out) # 110
+        out = F.relu(out)
+        out = self.conv2_2(out) # 110
+        out = F.relu(out)
+        out = self.maxpool2(out) # 56
+        
+        out = self.conv3_1(out) # 54
+        out = F.relu(out)
+        out = self.conv3_2(out) # 54
+        out = F.relu(out)
+        out = self.conv3_3(out) # 54
+        out = F.relu(out)
+        out = self.maxpool3(out) # 28
+        
+        out = self.conv4_1(out) # 26
+        out = F.relu(out)
+        out = self.conv4_2(out) # 26
+        out = F.relu(out)
+        out = self.conv4_3(out) # 26
+        out = F.relu(out)
+        out = self.maxpool4(out) # 14
+        
+        # out = self.conv5_1(out) # 12
+        # out = F.relu(out)
+        # out = self.conv5_2(out) # 12
+        # out = F.relu(out)
+        # out = self.conv5_3(out) # 12
+        # out = F.relu(out)
+        # out = self.maxpool5(out) # 7
+        
+        # 展平
+        # out = out.view(in_size, -1)
+        
+        # out = self.fc1(out)
+        # out = F.relu(out)
+        # out = self.fc2(out)
+        # out = F.relu(out)
+        # out = self.fc3(out)
+        
+        # out = F.log_softmax(out, dim=1)
+
+        return out
+
 class Net(torch.nn.Module):
     def __init__(self, batch, device, num_layers):
         super(Net, self).__init__()
         self.batch = batch
         self.device = device
-        self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
-        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
-        self.conv3 = nn.Conv2d(64, 64, 3, padding=1)
-        self.conv4 = nn.Conv2d(64, 32, 3, padding=1)
+        # self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
+        # self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
+        # self.conv3 = nn.Conv2d(64, 64, 3, padding=1)
+        # self.conv4 = nn.Conv2d(64, 32, 3, padding=1)
+        self.vgg16 = VGG16()
          # an affine operation: y = Wx + b
         self.num_layers = num_layers
-        self.gru1 = nn.GRU(32*16, 128, num_layers=self.num_layers, bidirectional=True, dropout=0.3)
+        # self.gru1 = nn.GRU(32*16, 128, num_layers=self.num_layers, bidirectional=True, dropout=0.3)
+        self.lstm = nn.LSTM(32*16, 128, num_layers=self.num_layers, bidirectional=True, dropout=0.3)
+
         self.fm = FeatureMap(self.batch)
         self.fc = nn.Linear(256, 66)
         #2*10  4*20 8*40 16*80 32*160
 
     def forward(self, x):
-        x = F.leaky_relu(self.conv1(x))
-        x = F.leaky_relu(self.conv2(x))
-        x = F.max_pool2d(x, (2, 2))
-        x = F.leaky_relu(self.conv3(x))
-        x = F.leaky_relu(self.conv4(x))
-        x = F.max_pool2d(x, (2, 2))
+        # x = F.leaky_relu(self.conv1(x))
+        # x = F.leaky_relu(self.conv2(x))
+        # x = F.max_pool2d(x, (2, 2))
+        # x = F.leaky_relu(self.conv3(x))
+        # x = F.leaky_relu(self.conv4(x))
+        # x = F.max_pool2d(x, (2, 2))
+        x = self.vgg16(x)
         x = self.fm(x)
         # 维度转换
         x = x.permute(1, 0, 2)
-        x, h = self.gru1(x)
+        # x, h = self.gru1(x)
+        x, h = self.lstm(x)
         x = self.fc(x)
         x = F.log_softmax(x, dim=2)
         return x
